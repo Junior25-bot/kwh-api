ccced8a5f799a05c475f9745b7097557864e216b
Junior Fruto <jrfruto19@hotmail.com>
2025-11-11 17:11:28 -0500

    Mejoras en ConsumoController y ConsumoRequest

--- src/main/java/com/kwh/kwhapi/model/User.java
+++ src/main/java/com/kwh/kwhapi/model/User.java
@@ -1,21 +1,42 @@
-
 package com.kwh.kwhapi.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import java.util.Set;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "usuarios")
 public class User {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
 
-  @Column(unique = true)
-  private String username;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  private String password;
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "rol")
+    private Set<String> roles;
 }
--- src/main/java/com/kwh/kwhapi/repository/TarifaRepository.java
+++ src/main/java/com/kwh/kwhapi/repository/TarifaRepository.java
@@ -2,9 +2,17 @@ package com.kwh.kwhapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.kwh.kwhapi.model.Tarifa;
 
+import java.time.LocalDate;
+
 @Repository
 public interface TarifaRepository extends JpaRepository<Tarifa, Long> {
+
+    @Query("SELECT t FROM Tarifa t WHERE :fecha BETWEEN t.fechaInicio AND t.fechaFin")
+    Tarifa findTarifaVigente(@Param("fecha") LocalDate fecha);
 }
+
