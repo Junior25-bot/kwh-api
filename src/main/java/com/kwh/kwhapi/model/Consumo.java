fbe7f34732dab774ef274c9ac6d9bb78113bbfd8
Junior Fruto <jrfruto19@hotmail.com>
2025-11-11 17:12:03 -0500

    Consumo actualizado

--- src/main/java/com/kwh/kwhapi/model/Consumo.java
+++ src/main/java/com/kwh/kwhapi/model/Consumo.java
@@ -1,23 +1,39 @@
 package com.kwh.kwhapi.model;
 
+
+
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "consumos")
 public class Consumo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private double consumo;
-    private double tarifa;
-    private double total;
-    private LocalDate fecha;
+    private double consumo; // kWh
+
+    private double tarifa; // valor unitario aplicado
+
+    private double total; // resultado del cálculo
+
+    private LocalDate fecha; // fecha del cálculo
+
+    public Consumo() {}
+
+    public Consumo(double consumo, double tarifa, double total, LocalDate fecha) {
+        this.consumo = consumo;
+        this.tarifa = tarifa;
+        this.total = total;
+        this.fecha = fecha;
+    }
 
     // Getters y setters
     public Long getId() {
@@ -56,5 +72,6 @@ public class Consumo {
         this.fecha = fecha;
     }
 }
+
 
 
